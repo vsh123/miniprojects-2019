ced56a2eb0372e42be7f18d49b4e7c77db0833d5
SunHo Van <vsh123@naver.com>
2019-09-09 14:36:51 +0900

    refactor: UploaderConfig 의존 위치 변경

--- src/main/java/techcourse/fakebook/utils/uploader/Uploader.java
+++ src/main/java/techcourse/fakebook/utils/uploader/Uploader.java
@@ -4,4 +4,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface Uploader {
     String upload(MultipartFile multipartFile, String dirName, String fileName);
+    String getArticlePath();
+    String getUserProfilePath();
+    String getUserProfileDefaultPath();
+    String getUserProfileDefaultName();
 }
